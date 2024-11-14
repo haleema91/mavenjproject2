45a1d5de3bb33459485d181a681efe9a3da5622d
Haleema Bushra <cutebushra8@gmail.com>
2024-11-14 11:06:04 +0530

    mvnproject

---
+++ src/main/java/com/org/kmec/mvnjavaproject/App.java
@@ -0,0 +1,13 @@
+package com.org.kmec.mvnjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
